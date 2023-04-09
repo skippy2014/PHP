a2a2d7332ac18993a19d01527bf48a9f3a48942f
Oskar Dubbelink <Oskardubbelink@gmail.com>
2023-04-09 15:36:40 +0200

    hoofdstuk 2

---
+++ H02/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php 
+  
+ # opdracht 1 Ik heb niet gefaald. Ik heb alleen 10000 manieren gevonden die niet werken.
+  $a = 'Ik heb niet gefaald';
+  $b = 'die niet werken';
+  $c = 10000;
+echo  $a.'. '.' Ik heb alleen '.$c.' manieren gevonden '.$b.'.'; 
+ echo '<br>';
+
+
+#opdracht 2 Mensen die geen fouten maken, werken niet.
+$d = 'die geen fout';
+$e = 'en';
+$f = 'niet.';
+echo 'Mens'.$e.' '.$d.$e.' mak'.$e.', '.'werk'.$e.' '.$f;
+echo '<br>';
+
+
+#opdracht 3 Try and fail, but never fail to try.
+$g = 'ry';
+$h = ', but';
+$i = 'fail';
+echo 'T'.$g.' and '.$i.$h.' never '.$i.' to t'.$g.'.';
+echo '<br>';
+
+
+#opdracht 4 De cursus “omgaan met teleurstellingen” kan vanavond helaas niet doorgaan.
+
+$j = 'omgaan';
+$k = 'met';
+$l = 'teleu';
+echo 'De cursus '.'"'.$j.' '.$k.' '.$l.'rstellingen"'.' kan vanavond helaas niet doorgaan.';
+
+
+
+  ?>
+</body>
+</html>
